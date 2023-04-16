7cadb2089c390d535f58f35d96e75a607107f983
Sridhar R <sridharofficialmail@gmail.com>
2023-04-16 20:44:08 +0530

    attendance get data by from and to

--- src/app/attendance/service/attendanceService.ts
+++ src/app/attendance/service/attendanceService.ts
@@ -1,21 +1,32 @@
 import { injectable } from "@leapjs/common";
-import { Attendance, AttendanceModel } from "../model/attendance";
+import { Attendance, AttendanceModel as attendance } from "../model/attendance";
 
 @injectable()
 class AttendanceService {
   public async postInTimeAttendance(employee: any, data: any): Promise<any> {
     data.employee = employee;
-    const registerAttendance = new AttendanceModel(data);
+    const registerAttendance = new attendance(data);
     const saveData = (await registerAttendance.save()) as Attendance;
     return saveData;
   }
   public async postOutTimeAttendance(employee: any, data: any): Promise<any> {
-    const updateAttendance = await AttendanceModel.updateOne({ employee }, { data });
+    const updateAttendance = await attendance.updateOne({ employee }, { data });
     return updateAttendance;
   }
   public async getAttendanceOfAEmployee(id: any): Promise<any> {
-    const data = await AttendanceModel.find({ employee: id });
+    const data = await attendance.find({ employee: id });
     return await data;
+  }
+  public async getAttendanceByDate(data: string = "02/09/2001") {
+    const date = new Date(data);
+    const result = await attendance.find({ date: date });
+    return result;
+  }
+  public async getAttendanceByFromDateAndToDate(fromString: string = "02/09/2001", toString: string = "02/09/2001") {
+    const from = new Date(fromString);
+    const to = new Date(toString);
+    const result = await attendance.find({ date: { $gte: { from }, $lte: { to } } });
+    return result;
   }
 }
 export { AttendanceService };

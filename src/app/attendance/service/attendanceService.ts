177018ac298c191dc306465569133e0b7c73cef5
Sridhar R <sridharofficialmail@gmail.com>
2023-04-17 10:49:11 +0530

    Refactor `attendanceService.ts` editing attendance, and date queries
    This commit improves the `attendanceService.ts` code, making it clearer and more efficient. The editAttendance function was refactored to accept parameters, and optimizations were made to date queries to use ISO strings.

--- src/app/attendance/service/attendanceService.ts
+++ src/app/attendance/service/attendanceService.ts
@@ -3,11 +3,11 @@ import { Attendance, AttendanceModel as attendance } from "../model/attendance";
 
 @injectable()
 class AttendanceService {
-    public async editAttendance(employee:string,dateString:string,data:Attendance){
-        const date=new Date(dateString)
-        const change=await attendance.findOneAndUpdateOne({date,employee},data)
-        return  await change;
-    }
+  public async editAttendance(employee: string, dateString: string, data: Attendance) {
+    const date = new Date(dateString);
+    const change = await attendance.findOneAndUpdateOne({ date, employee }, data);
+    return await change;
+  }
 
   public async postInTimeAttendance(employee: any, data: any): Promise<any> {
     data.employee = employee;
@@ -15,22 +15,26 @@ class AttendanceService {
     const saveData = (await registerAttendance.save()) as Attendance;
     return saveData;
   }
+
   public async postOutTimeAttendance(employee: any, data: any): Promise<any> {
     const updateAttendance = await attendance.updateOne({ employee }, { data });
     return updateAttendance;
   }
+
   public async getAttendanceOfAEmployee(id: any): Promise<any> {
     const data = await attendance.find({ employee: id });
     return await data;
   }
+
   public async getAttendanceByDate(data: string = "02/09/2001") {
-    const date = new Date(data);
+    const date = new Date(data).toISOString();
     const result = await attendance.find({ date: date });
     return result;
   }
+
   public async getAttendanceByFromDateAndToDate(fromString: string = "02/09/2001", toString: string = "02/09/2001") {
-    const from = new Date(fromString);
-    const to = new Date(toString);
+    const from = new Date(fromString).toISOString();
+    const to = new Date(toString).toISOString();
     const result = await attendance.find({ date: { $gte: { from }, $lte: { to } } });
     return result;
   }
